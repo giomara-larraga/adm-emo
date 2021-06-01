04e9ab25a241146cd01fcf591dc239de9979c33e
Bekir Afsar <bekir.afsar@gmail.com>
2021-06-02 00:08:25 +0300

    additional preference types

--- adm_emo/generatePreference.py
+++ adm_emo/generatePreference.py
@@ -84,6 +84,46 @@ def generateRP4decision(base: baseADM, max_assigned_vector):
     return reference_point
 
 
+def generatePerturbatedRP4decision(base: baseADM, max_assigned_vector):
+
+    assigned_vectors = base.assigned_vectors
+    theta = base.theta
+
+    ideal_cf = base.ideal_point
+
+    translated_cf = base.translated_front
+
+    sub_population_index = np.atleast_1d(
+        np.squeeze(np.where(assigned_vectors == max_assigned_vector))
+    )
+    sub_population_fitness = translated_cf[sub_population_index]
+
+    angles = theta[sub_population_index, max_assigned_vector]
+    # angles = np.divide(angles)
+    # print(angles)
+    # Distances of these solutions to the origin
+    sub_pop_fitness_magnitude = np.sqrt(
+        np.sum(np.power(sub_population_fitness, 2), axis=1)
+    )
+    # Index of the solution which has a minimum distance to the origin
+    minidx = np.where(sub_pop_fitness_magnitude == np.nanmin(sub_pop_fitness_magnitude))
+    distance_selected = sub_pop_fitness_magnitude[minidx]
+
+    aminidx = np.where(angles == np.nanmin(angles))
+    # print(aminidx)
+    # Create the reference point
+    reference_point = distance_selected[0] * base.vectors.values[max_assigned_vector]
+
+    reference_point = np.squeeze(reference_point + ideal_cf)
+
+    nearest = sub_population_fitness[aminidx] + ideal_cf
+    # print(nearest)
+    distance = np.linalg.norm(nearest - reference_point)
+    reference_point = np.squeeze(reference_point - distance)
+    # reference_point = reference_point + ideal_cf
+    return reference_point
+
+
 def generateRanges4learning(base: baseADM):
 
     ideal_cf = base.ideal_point
@@ -170,3 +210,76 @@ def generateRanges4decision(base: baseADM, max_assigned_vector):
     preferred_range = np.vstack((temp, temp2)).T
 
     return preferred_range, reference_point
+
+
+def preferredSolutions4learning(base: baseADM):
+
+    ideal_cf = base.ideal_point
+
+    translated_cf = base.translated_front
+
+    # Assigment of the solutions to the vectors
+    assigned_vectors = base.assigned_vectors
+
+    # Find the vector which has a minimum number of assigned solutions
+    number_assigned = np.bincount(assigned_vectors)
+    min_assigned_vector = np.atleast_1d(
+        np.squeeze(
+            np.where(
+                number_assigned == np.min(number_assigned[np.nonzero(number_assigned)])
+            )
+        )
+    )
+    sub_population_index = np.atleast_1d(
+        np.squeeze(np.where(assigned_vectors == min_assigned_vector[0]))
+        # If there are multiple vectors which have the minimum number of solutions, first one's index is used
+    )
+    # Assigned solutions to the vector which has a minimum number of solutions
+    sub_population_fitness = translated_cf[sub_population_index]
+
+    # Distances of these solutions to the origin
+    sub_pop_fitness_magnitude = np.sqrt(
+        np.sum(np.power(sub_population_fitness, 2), axis=1)
+    )
+    # Index of the solution which has a minimum distance to the origin
+
+    # k = sub_population_fitness.shape[0]
+    # print("num")
+    # print(k)
+    # print(sub_pop_fitness_magnitude.shape)
+    # minidx = np.argpartition(sub_pop_fitness_magnitude, k)
+
+    # print(minidx[:3])
+    solution_selected = sub_population_fitness
+
+    preferred_solution = np.squeeze(solution_selected + ideal_cf)
+
+    return preferred_solution
+
+
+def preferredSolutions4decision(base: baseADM, max_assigned_vector):
+
+    assigned_vectors = base.assigned_vectors
+
+    ideal_cf = base.ideal_point
+
+    translated_cf = base.translated_front
+
+    sub_population_index = np.atleast_1d(
+        np.squeeze(np.where(assigned_vectors == max_assigned_vector))
+    )
+    sub_population_fitness = translated_cf[sub_population_index]
+    # Distances of these solutions to the origin
+    sub_pop_fitness_magnitude = np.sqrt(
+        np.sum(np.power(sub_population_fitness, 2), axis=1)
+    )
+
+    # Index of the solution which has a minimum distance to the origin
+    minidx = np.argpartition(sub_pop_fitness_magnitude, 4)
+
+    # print(minidx[:3])
+    solution_selected = sub_population_fitness[minidx[:4]]
+
+    preferred_solution = np.squeeze(solution_selected + ideal_cf)
+
+    return preferred_solution
